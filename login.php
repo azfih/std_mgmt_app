154932fc789475e5d1c6aef083ed1a4885037a81
Hifza Rafiq <162020764+azfih@users.noreply.github.com>
2025-05-12 13:49:17 +0500

    Update login.php

--- login.php
+++ login.php
@@ -2,12 +2,12 @@
 session_start();
 include 'db.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
-    $stmt->bind_param("s", $email);
+    $stmt = $conn->prepare('SELECT * FROM users WHERE email = ?');
+    $stmt->bind_param('s', $email);
     $stmt->execute();
     $result = $stmt->get_result();
 
